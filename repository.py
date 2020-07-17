94221183c720ce4423ee943347f431c5c39f2d97
gillian.deville <gillian.deville@epita.fr>
2020-07-17 18:40:51 +0200

    [REPO][ADD]: can properly read the file

--- repository.py
+++ repository.py
@@ -1,13 +1,26 @@
 import csv
 
+# file: tickets_appels_201202.csv
 
 def load_csvfile(name):
 
     try:
-        file = open(name, newline='')
+        file = open(name, encoding='ISO-8859-1', newline='')
     except OSError as error:
-        raise Exception("error")
+        raise Exception("file not found")
 
     print("loading csv file: " + name)
+
+    fname = ["Compte facturé", "N° Facture", "N° abonné", "Date", "Heure", "Durée/volume réel", "Durée/volume facturé", "Type"]
+    reader = csv.DictReader(file, delimiter=";", fieldnames=fname)
+
+    for row in reader:
+
+        try:
+            print(row['N° Facture'])
+        except:
+            continue
+
+        i += 1
 
 
